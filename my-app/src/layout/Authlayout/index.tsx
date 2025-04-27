c52a22590b5339e9c08ad76fc7d718e8df2be031
adarsh2929 <adarshv2929@gmail.com>
2025-04-27 14:11:41 +0530

    c

--- my-app/src/layout/Authlayout/index.tsx
+++ my-app/src/layout/Authlayout/index.tsx
@@ -3,15 +3,14 @@ import {useEffect} from 'react';
 import {useNavigate,useLocation,Outlet} from 'react-router-dom';
 
 export const AuthLayout = () => {
-    const authPath = ['/login','/register','/booking','/room'];
+    const authPath = ['/booking','/room'];
     const navigate = useNavigate();
     const {pathname} = useLocation();
     const path = window.location.pathname;
 
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!authPath.includes(path) && !token) {
+        if (!authPath.includes(path)) {
             navigate('/room');
         }
 
